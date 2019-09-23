6ff1417f76207a769d2a87936d282c7498d9cd2d
Devin McFarlane <devin.ryan.mcfarlane@gmail.com>
2019-09-23 11:32:33 -0400

    Cleaned up code

--- dialog1.py
+++ dialog1.py
@@ -1,7 +1,7 @@
 import datetime
 name = input("What is your name?: ")
-birth_year = int(input("Great to meet you, " + name + "!  What year were you born?: "))
+birth_year = int(input(f"Great to meet you, {name}!  What year were you born?: "))
 now = datetime.datetime.now()
-numb1 = (now.year - birth_year) -1
-numb2 = now.year - birth_year
-print("Oh, " + str(birth_year) + ". That makes you either " + str(numb1) + " or " + str(numb2) + " years old.")
+a = (now.year - birth_year) -1
+b = now.year - birth_year
+print(f"Oh, {birth_year}. That makes you either {a} or {b} years old.")
