1ebc8616184c0ac40724a1bed8844f25fcbac1c3
Devin McFarlane <devin.ryan.mcfarlane@gmail.com>
2019-12-04 09:55:45 -0500

    Added ASCII Base64 encoding and decoding

--- exercises/base_tools.py
+++ exercises/base_tools.py
@@ -39,7 +39,7 @@ def to_base(num, base):
     return numstr
 
 
-def to_base64(num):
+def to_base64num(num):
     digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
     if num == 0:
         return "0"
@@ -50,4 +50,55 @@ def to_base64(num):
     return numstr
 
 
-print(to_base64(65535))
+def encode_base64(ys):
+    digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+    test_str = str(ys)
+    s = ""
+    for i in test_str:
+        i = format(ord(i), 'b')
+        if len(i) < 8:
+            i = ("0" * (8-len(i))) + i
+        s = s + i
+    o = []
+    fo = []
+    fs = ""
+    while s:
+        o.append(s[:6])
+        s = s[6:]
+    if not len(o[-1]) == 6:
+        o[-1] = o[-1] + "0"*(6-len(o[-1]))
+    for binary in o:
+        i = int(binary, 2)
+        fo.append(i)
+    for num in fo:
+        fs = fs + digits[num]
+    fs = fs + ("=" * (4 - (len(fs) % 4)))
+    return fs
+
+
+def spliter(word):
+    return [char for char in word]
+
+
+def decode_base64(ys):
+    digits = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/"
+    test_str = str(ys).replace('=', "")
+    strlist = spliter(test_str)
+    bins = ""
+    o = []
+    fo = []
+    for s in strlist:
+        loc = digits.find(s)
+        loc = format(loc, 'b')
+        if len(loc) < 6:
+            loc = str(loc)
+            loc = ("0" * (6 - len(loc))) + loc
+        bins = bins + loc
+    while bins:
+        o.append(bins[:8])
+        bins = bins[8:]
+    for s in o:
+        if len(s) == 8:
+            fo.append(s)
+    fs = ''.join([chr(int(binary, 2)) for binary in fo])
+    return fs
